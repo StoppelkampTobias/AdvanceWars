3331ceb25125e0c30392af37645da903ab2ec389
stoppeltob <stoppelkamp05@gmail.com>
2024-05-03 10:19:44 +0200

    erste files

---
+++ src/main/java/org/App.java
@@ -0,0 +1,17 @@
+package org;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Advance Wars");
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/org/MainClass.java
@@ -0,0 +1,7 @@
+package org;
+
+import javafx.application.Application
+
+public class MainClass {
+
+}
